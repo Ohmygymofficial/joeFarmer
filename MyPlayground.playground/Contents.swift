ef95e2f0be03085da00576cc0a78778e86764902
ohmygymofficial <contact@ohmygym.fr>
2019-03-15 22:21:15 +0100

    Just a playground with exercice to play with VAR LET and FOR...IN

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,12 @@
+var money : Double = 0
+
+for _ in 1...30 {
+    
+    //Joe nourrit la vache, cout 4e
+    money -=  4
+    // Joe vend son lait
+    money += 0.50 * 30
+    
+}
+
+print("\(money) Euro")
