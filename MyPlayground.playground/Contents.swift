0708602df7c84439d476151cc131b324d894119a
ohmygymofficial <contact@ohmygym.fr>
2019-03-16 18:42:30 +0100

    Update Joe Farmer with WHILE and with IF

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,12 +1,37 @@
-var money : Double = 0
+//DECLARATIONS DES CONSTANTES ET VARIABLES
+// le prix du voyage
+let price = 1499.0
 
-for _ in 1...30 {
+// le nombre de jours pendant lesquels je dois economiser
+var numberOfDay = 0
+
+// Tableau avec lait, Ble, Farine
+var barn = [0,0,0]
+
+// L'argent de JOE (au premier jour)
+var money = 0.0
+
+//calcul de ce que JOE gagne et depense
+while money < price {
+    //joe nourrit les vaches tous les jours
+    money -= 4
     
-    //Joe nourrit la vache, cout 4e
-    money -=  4
-    // Joe vend son lait
-    money += 0.50 * 30
-    
+    //sur certains jours du mois, Joe vends differentes choses
+    if numberOfDay % 30 == 1 {
+        // Joe moissone
+        money += 100 * 0.30
+    } else if numberOfDay % 30 == 10 || numberOfDay % 30 == 20 {
+        // Joe tond les moutons
+        money += 30 * 1
+    } else {
+        // Joe vends son lait
+        money += 30 * 0.50
+    }
+    // on passe au jour suivant
+    numberOfDay += 1
+        
 }
 
-print("\(money) Euro")
+
+print(money)
+
